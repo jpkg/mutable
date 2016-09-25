5ff5bc920bc91bac98a07e4e57c7b4c79452e020
minerguy31 <minerguy31@gmail.com>
2016-09-25 13:27:58 -0600

    Initial Commit

---
+++ src/jpkg/mutable/MutableInteger.java
@@ -0,0 +1,83 @@
+package jpkg.mutable;
+
+import java.io.Serializable;
+
+public class MutableInteger implements Serializable, Comparable<MutableInteger> {
+
+	private static final long serialVersionUID = -2285917765136376454L;
+	
+	/**
+	 * The value of this MutableInteger
+	 */
+	private int val;
+
+	/**
+	 * Make a new MutableInteger with an initial value
+	 * @param val initial value
+	 */
+	public MutableInteger(int val) {
+		this.val = val;
+	}
+
+	/**
+	 * Get the value of this MutableInteger
+	 * @return
+	 */
+	public int get() {
+		return val;
+	}
+
+	/**
+	 * Set the value of this MutableInteger
+	 * @param val new value
+	 */
+	public void set(int val) {
+		this.val = val;
+	}
+	
+	/**
+	 * Does what it says on the tin. Increments value of this MutableInteger.
+	 */
+	public void increment() {
+		val++;
+	}
+	
+	/**
+	 * Does what it says on the tin. Decrements value of this MutableInteger.
+	 */
+	public void decrement() {
+		val--;
+	}
+	
+	/**
+	 * Change the value by amount
+	 * @param delta
+	 * @return the value, for reference
+	 */
+	public int changeBy(int delta) {
+		val += delta;
+		return val;
+	}
+	
+	@Override
+	public String toString() {
+		return Integer.toString(val);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Integer.hashCode(val);
+	}
+	
+	@Override
+	public boolean equals(Object another) {
+		if(another instanceof MutableInteger) {
+			return ((MutableInteger) another).val == this.val;
+		} else return false;
+	}
+
+	@Override
+	public int compareTo(MutableInteger arg0) {
+		return Integer.compare(this.val, arg0.val);
+	}
+}
---
+++ src/jpkg/mutable/MutableLong.java
@@ -0,0 +1,83 @@
+package jpkg.mutable;
+
+import java.io.Serializable;
+
+public class MutableLong implements Serializable, Comparable<MutableLong> {
+	
+	private static final long serialVersionUID = 361520893000778060L;
+	
+	/**
+	 * The value of this MutableLong
+	 */
+	private long val;
+
+	/**
+	 * Make a new MutableLong with an initial value
+	 * @param val initial value
+	 */
+	public MutableLong(long val) {
+		this.val = val;
+	}
+
+	/**
+	 * Get the value of this MutableLong
+	 * @return
+	 */
+	public long get() {
+		return val;
+	}
+
+	/**
+	 * Set the value of this MutableLong
+	 * @param val new value
+	 */
+	public void set(long val) {
+		this.val = val;
+	}
+	
+	/**
+	 * Does what it says on the tin. Increments value of this MutableLong.
+	 */
+	public void increment() {
+		val++;
+	}
+	
+	/**
+	 * Does what it says on the tin. Decrements value of this MutableLong.
+	 */
+	public void decrement() {
+		val--;
+	}
+	
+	/**
+	 * Change the value by amount
+	 * @param delta
+	 * @return the value, for reference
+	 */
+	public long changeBy(long delta) {
+		val += delta;
+		return val;
+	}
+	
+	@Override
+	public String toString() {
+		return Long.toString(val);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Long.hashCode(val);
+	}
+	
+	@Override
+	public boolean equals(Object another) {
+		if(another instanceof MutableLong) {
+			return ((MutableLong) another).val == this.val;
+		} else return false;
+	}
+
+	@Override
+	public int compareTo(MutableLong arg0) {
+		return Long.compare(this.val, arg0.val);
+	}
+}
